e862bfe3fcf946e5f90efc4a7b01f952598b1a48
Joaquin Vettorazzi <j.vettorazzi95@gmail.com>
2022-11-30 21:02:07 -0300

    Mis primeros familiares agregados a tabla

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
+
 
 def index(request):
     return render (request, "ejemplo/saludar.html")
@@ -17,3 +19,11 @@ def sumar(request, a, b):
      "resultado": a + b
     }
     )
+
+def buscar(request):
+    query = request.GET["q"]
+    return render(request, "ejemplo/buscar.html", {"query": query})
+
+def mostrar_familiares(request):
+    lista_familiares = Familiar.objects.all()
+    return render(request, "ejemplo/familiares.html", {"lista_familiares" : lista_familiares})
