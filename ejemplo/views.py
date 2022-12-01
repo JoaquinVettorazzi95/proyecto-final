6e4dc01b73809a6c315a59d9ee9cf1d406ebeaff
Joaquin Vettorazzi <j.vettorazzi95@gmail.com>
2022-12-01 20:06:04 -0300

    Agregue la parte la seccion buscar

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -21,8 +21,15 @@ def sumar(request, a, b):
     )
 
 def buscar(request):
+    lista_de_nombre = ["German", "Joaquin", "Andres", "Nicolas"]
     query = request.GET["q"]
-    return render(request, "ejemplo/buscar.html", {"query": query})
+   
+    if query in lista_de_nombre:
+        indice_del_resultado = lista_de_nombre.index(query)
+        resultado = lista_de_nombre[indice_del_resultado]
+    else:
+        resultado = "No hay match"
+    return render(request, "ejemplo/buscar.html", {"resultado": resultado})
 
 def mostrar_familiares(request):
     lista_familiares = Familiar.objects.all()
